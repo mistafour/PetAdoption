02292a9e6ac77b29aacc0a85485451049f645eca
mistafour <edokens44@gmail.com>
2026-07-18 19:01:17 +0100

    created ACM certificate for Domain and ELB for the servers

--- main.tf
+++ main.tf
@@ -688,3 +688,151 @@ resource "aws_lb_listener" "pet-lb-listener" {
     target_group_arn = aws_lb_target_group.target-group-lb-HTTP.arn
   }
 }
+# ACM Certificate for pet domain
+resource "aws_acm_certificate" "pet_certificate" {
+  domain_name               = var.domain_name
+  subject_alternative_names = ["*.${var.domain_name}"]
+  validation_method         = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+#creating stage elb
+
+resource "aws_elb" "elb-jenkins" {
+
+name = "elb-jenkins"
+
+security_groups = [aws_security_group.pet_jenkins_sg.id]
+
+subnets = [aws_subnet.pet-pubsub-1.id, aws_subnet.pet-pubsub-2.id]
+
+
+ listener {
+
+instance_port = 8080
+
+instance_protocol = "http"
+
+lb_port = 443
+
+lb_protocol = "https"
+
+ssl_certificate_id = aws_acm_certificate.pet_certificate.arn
+
+ }
+
+
+
+health_check {
+
+healthy_threshold = 2
+
+unhealthy_threshold = 2
+
+timeout = 3
+
+target = "tcp:8080"
+
+interval = 30
+
+ }
+
+
+
+instances = [aws_instance.pet-jenkins.id]
+
+cross_zone_load_balancing  = true
+
+idle_timeout = 400
+
+connection_draining = true
+
+connection_draining_timeout = 400
+
+ tags = {
+
+Name = "elb-jenkins"
+
+ }
+
+}
+
+# Creating ELB for Nexus
+resource "aws_elb" "elb-nexus" {
+  name = "elb-nexus"
+
+  security_groups = [aws_security_group.pet_nexus_sg.id]
+
+  subnets = [
+    aws_subnet.pet-pubsub-1.id,
+    aws_subnet.pet-pubsub-2.id
+  ]
+
+  listener {
+    instance_port      = var.nexus_port
+    instance_protocol  = "http"
+    lb_port            = 443
+    lb_protocol        = "https"
+    ssl_certificate_id = aws_acm_certificate.pet_certificate.arn
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "TCP:${var.nexus_port}"
+    interval            = 30
+  }
+
+  instances = [aws_instance.nexus.id]
+
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "elb-nexus"
+  }
+}
+
+# Creating ELB for SonarQube
+resource "aws_elb" "elb-sonarqube" {
+  name = "elb-sonarqube"
+
+  security_groups = [aws_security_group.pet_sonarqube_sg.id]
+
+  subnets = [
+    aws_subnet.pet-pubsub-1.id,
+    aws_subnet.pet-pubsub-2.id
+  ]
+
+  listener {
+    instance_port      = var.sonar_port
+    instance_protocol  = "http"
+    lb_port            = 443
+    lb_protocol        = "https"
+    ssl_certificate_id = aws_acm_certificate.pet_certificate.arn
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "TCP:${var.sonar_port}"
+    interval            = 30
+  }
+
+  instances = [aws_instance.pet-sonarqube.id]
+
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "elb-sonarqube"
+  }
+}
